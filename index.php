497bf9a3ba2ee95c4bf2ab9bef9b2922d513084b
Lucas <lucasmarion.dev@gmail.com>
2025-12-22 11:00:40 -0300

    Código inicial: recuperando informações do banco de dados e exibindo na tela

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<pre>
 <?php
 
-echo "Hello, world!";
+// CONECTANDO COM O BANCO DE DADOS ATRAVÉS DA CLASSE PDO
+$dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
+
+
+$pdo = new PDO(
+                $dsn,
+                "product_db_user",
+                "secret",
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+            );
+
+$stmt = $pdo->query("SELECT * FROM product");
+
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($products);

5d5c04bfeb8b3e3e06ca9fb36a932cb544fded71
Lucas <lucasmarion.dev@gmail.com>
2025-12-23 09:43:38 -0300

    Criação de uma view para se responsabilizar pela visualização em HTML. Agora, o index só possui código PHP.

--- index.php
+++ index.php
@@ -4,27 +4,6 @@ require "model.php";
 
 $products = (new Model)->getData();
 
+require "view.php";
+
 ?>
-
-<!DOCTYPE html>
-<html>
-
-    <head>
-        <title>Products</title>
-        <meta charset="UTF-8">
-    </head>
-
-    <body>
-        
-        <h1>Products</h1>
-		
-        <?php foreach ($products as $product): ?>
-        
-			<h2><?= htmlspecialchars($product["name"]) ?></h2>
-			<p><?= htmlspecialchars($product["description"]) ?></p>
-		
-        <?php endforeach; ?>
-            
-    </body>
-
-</html>

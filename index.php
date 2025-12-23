f58351df7b56065c728dff9b456a4efdfabec78c
Lucas <lucasmarion.dev@gmail.com>
2025-12-23 13:19:34 -0300

    Criando controllers e executando actions através da URL com queries

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
-require "src/controllers/products.php";
+$controller = $_GET["controller"];
 
-(new Products)->index();
+if ($controller === "products") {
+	require "src/controllers/products.php";
+	$controller_object = new Products;
+}
+elseif ($controller === "home") {
+	require "src/controllers/home.php";
+	$controller_object = new Home;
+}
+
+$action = $_GET["action"];
+
+if ($action === "index") {
+	$controller_object->index();
+}
+elseif ($action === "show") {
+	$controller_object->show();
+}

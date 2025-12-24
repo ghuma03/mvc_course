769c388853e51a2b88b0059137021df914d28682
Lucas <lucasmarion.dev@gmail.com>
2025-12-24 10:46:47 -0300

    Alteração no servidor: agora o projeto está usando um virtualhost para acessa uma pasta externa à htdocs. Criação de .htacess para que a URL possa ser modificada. Início de convenção de estrutur de URL para rotear o funcionamento do sistema

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
-$controller = $_GET["controller"];
-require "src/controllers/$controller.php";
+$segments = explode("/", parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
 
-$action = $_GET["action"];
+$controller = $segments[1];
+$action = $segments[2];
+
+require "src/controllers/$controller.php";
 
 $controller_obj = new $controller;
 	$controller_obj->$action();

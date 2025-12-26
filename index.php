a14ab517036613ce1063af83dda812ace200a26d
Lucas <lucasmarion.dev@gmail.com>
2025-12-26 10:08:40 -0300

    Criação e adesão de um Router para lidar com a manipulação de URLs e encontrar o controller e sua action a ser executada

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
-$segments = explode("/", parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
+require "src/router.php";
 
-$controller = $segments[1];
-$action = $segments[2];
+$router = new Router;
+	$router->add("/home/index", array("controller" => "home", "action" => "index"));
+	$router->add("/products", array("controller" => "products", "action" => "index"));
+	$router->add("/", array("controller" => "home", "action" => "index"));
+	
+$params = $router->match(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
+
+if ($params === false) {
+	echo "No route matched!";
+	die();
+}
+
+$controller = $params["controller"];
+$action = $params["action"];
 
 require "src/controllers/$controller.php";
 
